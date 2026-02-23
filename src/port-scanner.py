3a33a9e156f566b52c1967d83148be1bec749fbb
fredd-deez <soksophal2004@gmail.com>
2026-02-23 23:21:39 +0700

    Added top 100, 500, and 1000 TCP ports in a separate file

--- src/port-scanner.py
+++ src/port-scanner.py
@@ -5,6 +5,7 @@ import subprocess
 import platform
 import concurrent.futures
 import time
+from components.ports import TOP_100, TOP_500, TOP_1000
 
 # Terminal Colors
 class Color:
@@ -20,25 +21,16 @@ class ColoredIcons:
   ERROR = f"{Color.RED}[!]{Color.RESET}"
   INFO = f"{Color.BLUE}[*]{Color.RESET}"
 
-TOP_100_PORTS = [
-    7, 9, 13, 21, 22, 23, 25, 26, 37, 53, 79, 80, 81, 88, 106, 110, 111, 113, 119, 
-    135, 139, 143, 144, 179, 199, 389, 427, 443, 444, 445, 465, 513, 514, 515, 543, 
-    544, 548, 554, 587, 631, 646, 873, 990, 993, 995, 1025, 1026, 1027, 1028, 1029, 
-    1110, 1433, 1720, 1723, 1755, 1900, 2000, 2001, 2049, 2121, 2717, 3000, 3128, 
-    3306, 3389, 3986, 4899, 5000, 5009, 5051, 5060, 5190, 5357, 5432, 5631, 5666, 
-    5800, 5900, 6000, 6001, 6646, 7070, 8000, 8008, 8009, 8080, 8081, 8443, 8888, 
-    9100, 9999, 10000, 32768, 49152, 49153, 49154, 49155, 49156, 49157
-]
-
 def get_args():
   parser = argparse.ArgumentParser(
       description="Simple Python-Based Port Scanner.",
-      epilog="Usage: python port-scanner.py scanme.nmap.org"
+      epilog="Usage: python port-scanner.py scanme.nmap.org",
+      formatter_class=argparse.RawTextHelpFormatter
       )
 
   parser.add_argument("target", help="The target IP address or hostname to scan") # .target
-  parser.add_argument("-p", "--ports", help="The target ports (e.g., 80, 21,22,23, 1-100)", default="top100") # .ports
-  parser.add_argument("-e", "--exclude", help="Ports to exclude from scanning (e.g., 80, 21,22,23, 1-100)") # .exclude
+  parser.add_argument("-p", "--ports", help="The target ports (e.g., 80, 21,22,23, 1-100).\n-p- to scan all 65535 ports (1-65535)\ntop100 to scan Nmap's top 100 most common TCP ports\ntop500 to scan Nmap's top 500 most common TCP ports\ntop1000 to scan Nmap's top 1000 most common TCP ports", default="top100") # .ports
+  parser.add_argument("-e", "--exclude", help="Ports to exclude from scanning (e.g., 80, 21,22,23, 1-100)", default="") # .exclude
   parser.add_argument("-Pn", help="Disable host discovery, treating all hosts as online", action="store_true") # .Pn
   parser.add_argument("-v", help="More verbose", action="store_true") # .v
   parser.add_argument("-t", "--threads", help="Number of threads for concurrent scanning (default: 4)", type=int, default=4) # .threads
@@ -48,8 +40,17 @@ def get_args():
 
 def parse_ports(port_args: str) -> list:
   if port_args == "top100":
-    print(f"{ColoredIcons.INFO} No port given. Defaulting to Nmap's Top 100 most common TCP ports")
-    return TOP_100_PORTS
+    print(f"{ColoredIcons.INFO} Scanning Nmap's Top 100 TCP ports")
+    return TOP_100
+  elif port_args == "top500":
+    print(f"{ColoredIcons.INFO} Scanning Nmap's Top 500 TCP ports")
+    return TOP_500
+  elif port_args == "top1000":
+    print(f"{ColoredIcons.INFO} Scanning Nmap's Top 1000 TCP ports")
+    return TOP_1000
+  elif port_args == "-":
+    print(f"{ColoredIcons.INFO} Scanning all 65535 ports (1-65535)")
+    return list(range(1, 65536))
 
   ports = []
   try:
